01065cd5979b2ec45f131937acf09e7f9d7aecd6
visshnnu-xseed <85931077+visshnnu-xseed@users.noreply.github.com>
2021-09-23 13:35:33 +0530

    change model structure

--- models/aboutModel.js
+++ models/aboutModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const aboutSchema = mongoose.Schema(
+const hobbiesSchema = mongoose.Schema(
 	{
 		title: {
 			type: String,
@@ -9,6 +9,65 @@ const aboutSchema = mongoose.Schema(
 		description: {
 			type: String,
 			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const aboutSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			require: true,
+		},
+		designation: {
+			type: String,
+			required: true,
+		},
+		photo: {
+			type: String,
+			required: true,
+		},
+		resumeLink: {
+			type: String,
+			required: true,
+		},
+		aboutDescription: {
+			type: String,
+			required: true,
+		},
+		mobile: {
+			type: String,
+			require: true,
+		},
+		Email: {
+			type: String,
+			required: true,
+		},
+		linkedinUrl: {
+			type: String,
+			required: true,
+		},
+		twitterUrl: {
+			type: String,
+			required: true,
+		},
+		instagramUrl: {
+			type: String,
+			required: true,
+		},
+		facebookUrl: {
+			type: String,
+			required: true,
+		},
+		githubUrl: {
+			type: String,
+			required: true,
+		},
+		hobbies: {
+			type: [hobbiesSchema],
 		},
 	},
 	{
